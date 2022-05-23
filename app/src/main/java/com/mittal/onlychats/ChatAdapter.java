0ba5ce98f90430b1596462bfedb7a063f41efc4a
Thebrowngeek-Sudhanshu <sudhanshu200002@gmail.com>
2022-05-23 11:25:07 +0530

    second commit

--- app/src/main/java/com/mittal/onlychats/ChatAdapter.java
+++ app/src/main/java/com/mittal/onlychats/ChatAdapter.java
@@ -1,4 +1,75 @@
 package com.mittal.onlychats;
 
-public class ChatAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ChatAdapter extends RecyclerView.Adapter {
+    ArrayList<ChatList> chatLists;
+    Context context;
+    int SENDER_VIEW_TYPE=1;
+    int RECEIVER_VIEW_TYPE=2;
+    public ChatAdapter(ArrayList<ChatList> chatLists, Context context) {
+        this.chatLists = chatLists;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ReceiverViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView receiverMsg,receiverTime;
+
+
+
+
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            receiverMsg=itemView.findViewById(R.id.receiverText);
+            receiverTime=itemView.findViewById(R.id.receiverTime);
+
+
+
+        }
+    }
+    public class SenderViewHolder extends RecyclerView.ViewHolder {
+
+
+        TextView senderMsg,senderTime;
+
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            senderMsg=itemView.findViewById(R.id.senderText);
+            senderTime=itemView.findViewById(R.id.senderTime);
+
+
+
+        }
+    }
 }
--- app/src/main/java/com/mittal/onlychats/ChatList.java
+++ app/src/main/java/com/mittal/onlychats/ChatList.java
@@ -1,4 +1,27 @@
 package com.mittal.onlychats;
 
 public class ChatList {
+
+    String uId,message;
+    Long timestamp;
+
+    public ChatList(String uId, String message, Long timestamp) {
+        this.uId = uId;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public ChatList(String uId, String message) {
+        this.uId = uId;
+        this.message = message;
+    }
+    public ChatList(){}
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
 }
